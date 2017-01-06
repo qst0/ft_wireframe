bd78292ae6155850d83d00fc3b4b31b159c5e92a
Mason Young <masondyoung@gmail.com>
2017-01-05 16:58:23 -0800

    controls print out in console on startup

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,14 +6,28 @@
 /*   By: myoung <myoung@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 21:22:20 by myoung            #+#    #+#             */
-/*   Updated: 2016/11/04 21:22:26 by myoung           ###   ########.fr       */
+/*   Updated: 2017/01/05 16:57:17 by myoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
-void	fdf(t_view *view)
+static void		show_controls(void)
 {
+	ft_putstr("w/s : rotate by x axis\n"
+			"a/d : rotate by y axis\n"
+			"q/e : rotate by z axis\n"
+			"\n"
+			"c   : trippy colors\n"
+			"ijkl: translate (move around)\n"
+			"+/- : zoom / scale\n"
+			"with z x or y, scale that axis\n"
+			"Esc to quit.\n");
+}
+
+void			fdf(t_view *view)
+{
+	show_controls();
 	init_color_table(view, 100);
 	full_redraw(view);
 	view_set_hooks(view);
