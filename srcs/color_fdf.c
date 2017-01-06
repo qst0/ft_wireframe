b98813b2d094d0dbdb00521295177fe8663a0282
Mason Young <masondyoung@gmail.com>
2017-01-05 16:48:34 -0800

    clean up

--- srcs/color_fdf.c
+++ srcs/color_fdf.c
@@ -6,19 +6,16 @@
 /*   By: myoung <myoung@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/05 16:20:54 by myoung            #+#    #+#             */
-/*   Updated: 2017/01/05 16:41:08 by myoung           ###   ########.fr       */
+/*   Updated: 2017/01/05 16:48:08 by myoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
-#include <stdio.h>
-
 void		color_hook(t_view *view)
 {
 	if (view->pressed->c)
 	{
-		printf("Hey!");
 		view->r = rand() % 3;
 		view->g = rand() % 3;
 		view->b = rand() % 3;
